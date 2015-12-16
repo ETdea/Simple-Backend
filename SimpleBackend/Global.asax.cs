bc5bff82a52186789a9d83889ac1452183060ec6
Creator <Creator@ourpower.com.tw>
2015-12-16 14:57:30 +0800

    add Application_Error REMARK

--- SimpleBackend/Global.asax.cs
+++ SimpleBackend/Global.asax.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -20,5 +17,50 @@ namespace SimpleBackend
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             DatabaseConfig.RegisterDbContext();
         }
+
+        //void Application_Error(object sender, EventArgs e)
+        //{
+        //    // 發生未處理錯誤時執行的程式碼
+
+        //    var error = Server.GetLastError();
+        //    Server.Transfer("~/error.html");
+        //}
+
+        //protected void Application_Error(object sender, EventArgs e)
+        //{
+        //    Exception exception = Server.GetLastError();
+        //    Response.Clear();
+
+        //    HttpException httpException = exception as HttpException;
+
+        //    string action = string.Empty;
+
+        //    if (httpException != null)
+        //    {
+        //        switch (httpException.GetHttpCode())
+        //        {
+        //            case 404:
+        //                // page not found 
+        //                action = "NotFound";
+        //                break;
+        //            //TODO: handle other codes
+        //            default:
+        //                action = "general-error";
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //TODO: Define action for other exception types
+        //        action = "general-error";
+        //    }
+
+        //    Server.ClearError();
+
+        //    string culture = Thread.CurrentThread.CurrentCulture.Name;
+
+        //    Exception exception = Server.GetLastError();
+        //    Response.Redirect(String.Format("~/{0}/error/{1}", culture, action));
+        //}
     }
 }
